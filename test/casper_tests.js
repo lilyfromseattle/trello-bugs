a42566659faf50329e27770baf290e6d55810e72
Lily Pace <lilyfpace@gmail.com>
2015-03-26 15:50:22 -0700

    messing with package.json

--- test/casper_tests.js
+++ test/casper_tests.js
@@ -1,28 +1,28 @@
-// var casper = require('casper').create();
-// phantom.casperTest = true;
-//
-// casper.test.begin('Trello Bug counter has working form', function suite(test) {
-//   casper.start('http://trello-bug-counter.herokuapp.com', function() {
-//       this.test.assertTextExists('Trello', 'page body contains "trello"');
-//       this.test.assertTitle("Trello Bug Counter", 'Trello Bug Counter homepage title is the one expected');
-//       this.test.assertExists('form[action="/"]', 'date form is found');
-//       this.fill('form[action="/"]', {
-//           startdate: 'January 1, 2012',
-//           enddate: 'March 15, 2015',
-//       }, true);
-//   });
-//
-//   casper.then(function() {
-//       this.test.assertTitle("", 'no title for results page yet!');
-//       // this.test.assertUrlMatch(/q=foo/, 'search term has been submitted');
-//       this.test.assertTextExists('Total', 'page body contains "total"');
-//       this.test.assertVisible('h3.text-center');
-//       this.test.assertExists('div.title-div');
-//       this.test.assertEval(function() {
-//           return __utils__.findAll('td.text-center').length >= 8;
-//       }, 'search for bugs retrieves 8 or more results');
-//   });
-// });
-// casper.run(function() {
-//     this.test.renderResults(true);
-// });
+var casper = require('casper').create();
+phantom.casperTest = true;
+
+casper.test.begin('Trello Bug counter has working form', function suite(test) {
+  casper.start('http://trello-bug-counter.herokuapp.com', function() {
+      this.test.assertTextExists('Trello', 'page body contains "trello"');
+      this.test.assertTitle("Trello Bug Counter", 'Trello Bug Counter homepage title is the one expected');
+      this.test.assertExists('form[action="/"]', 'date form is found');
+      this.fill('form[action="/"]', {
+          startdate: 'January 1, 2012',
+          enddate: 'March 15, 2015',
+      }, true);
+  });
+
+  casper.then(function() {
+      this.test.assertTitle("", 'no title for results page yet!');
+      // this.test.assertUrlMatch(/q=foo/, 'search term has been submitted');
+      this.test.assertTextExists('Total', 'page body contains "total"');
+      this.test.assertVisible('h3.text-center');
+      this.test.assertExists('div.title-div');
+      this.test.assertEval(function() {
+          return __utils__.findAll('td.text-center').length >= 8;
+      }, 'search for bugs retrieves 8 or more results');
+  });
+});
+casper.run(function() {
+    this.test.renderResults(true);
+});
