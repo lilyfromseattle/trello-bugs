7ba72bd8413ee385aa796402d5dcfcd1348050bb
Lily Pace <lilyfpace@gmail.com>
2015-03-30 10:54:28 -0700

    commented out more tests

--- test/casper/casper_tests.js
+++ test/casper/casper_tests.js
@@ -2,14 +2,15 @@ var process = require('system').env;
 var testURI = (process.server_uri) ? process.server_uri : "http://localhost:5000";
 // var testURI = "http://trello-bug-counter.herokuapp.com";
 var casper = require('casper').create();
-console.log(testURI);
-console.log("HEY!");
-console.log(process.server_uri);
+// console.log(testURI);
+// console.log("HEY!");
+// console.log(process.server_uri);
 phantom.casperTest = true;
 
 casper.test.begin('Trello Bug counter has working form',9, function suite(test) {
   casper.start(testURI, function() {
-    test.assert(true, "true's true");
+    console.log("HI")
+    // test.assert(true, "true's true");
       // this.test.assertTextExists('Trello', 'page body contains "trello"');
       // this.test.assertTitle("Trello Bug Counter", 'Trello Bug Counter homepage title is the one expected');
       // this.test.assertExists('form[action="/"]', 'date form is found');
@@ -18,7 +19,7 @@ casper.test.begin('Trello Bug counter has working form',9, function suite(test) 
       //     enddate: 'March 15, 2015',
       // }, true);
   });
-  // 
+  //
   // casper.then(function() {
   //     this.test.assertTextExists('Total', 'page body contains "total"');
   //     this.test.assertVisible('h3.text-center');
