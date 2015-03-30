cb7adef4447da7df17b88fbdc01e4608363c64ae
Lily Pace <lilyfpace@gmail.com>
2015-03-30 13:59:57 -0700

    changed base to ./trello-bugs

--- gruntfile.js
+++ gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
 	         server: {
 		           options: {
 										hostname: "localhost",
-			              base: '/home/travis/build/lilyfromseattle/trello-bugs',
+			              base: './trello-bugs',
 			              port: 5000
 		                }
 	              }
