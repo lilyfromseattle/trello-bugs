1351a96dfa9aae5040523c610099ad243b6a8c5c
Lily Pace <lilyfpace@gmail.com>
2015-04-01 16:15:02 -0700

    gruntfile.js to grunt.js

---
+++ grunt.js
@@ -0,0 +1,18 @@
+
+// 'use strict';
+module.exports = function(grunt) {
+  grunt.initConfig({
+      'heroku-deploy' : {
+        deployBranch: 'test-branch'
+          // production : {
+          //     deployBranch : 'prod'
+          // },
+          // staging : {
+          //     deployBranch : 'staging'
+          // }
+      }
+  });
+
+  grunt.loadNpmTasks('grunt-heroku-deploy');
+  grunt.registerTask('deploy', 'heroku-deploy');
+};
