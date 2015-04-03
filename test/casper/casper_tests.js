681037d23870173ad701c8aaab8e77f1e9b3df4e
Lily Pace <lilyfpace@gmail.com>
2015-04-03 11:50:25 -0700

    fixed failing test

--- test/casper/casper_tests.js
+++ test/casper/casper_tests.js
@@ -9,7 +9,7 @@ phantom.casperTest = true;
 casper.test.begin('Trello Bug counter has working form',9, function suite(test) {
   casper.start(testURI, function() {
     test.assert(true, "true's true");
-    this.test.assertTitle("Trello Bug Counter BAHHHH", 'Trello Bug Counter homepage title is the one expected');
+    this.test.assertTitle("Trello Bug Counter", 'Trello Bug Counter homepage title is the one expected');
     this.test.assertTextExists('Trello', 'page body contains "trello"');
     this.test.assertExists('form[action="/"]', 'date form is found');
     this.fill('form[action="/"]', {
